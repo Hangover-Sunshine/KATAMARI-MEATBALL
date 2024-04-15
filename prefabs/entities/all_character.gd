0419c35613f1e54eb416f8b4b967e32f2b1d0f78
SBrianZ <brian@brianzavala.com>
2024-04-14 19:58:23 -0700

    NPC Visuals Randomizer
    Depending on character variable, all_character prefab will pick who is being drawn and select an outfit from them that makes sense.

---
+++ prefabs/entities/all_character.gd
@@ -0,0 +1,100 @@
+extends Node2D
+
+@onready var cult_heads = $All_Skeleton/Heads/Cult_Heads
+@onready var hero_heads = $All_Skeleton/Heads/Hero_Heads
+@onready var peon_heads = $All_Skeleton/Heads/Peon_Heads
+
+@onready var all_hands = $All_Skeleton/AllHands
+
+@onready var all_body = $All_Skeleton/AllBody
+@onready var peon_aapron = $All_Skeleton/Peon_Aapron
+
+# 0 - Player | 1 - Cultist | 2 - HERO Guard 
+# 3 - HERO Archer | 4 - HERO Wizard | 5 - Peon
+var character = 5
+
+# 0 - Pink | 1 - Ginger | 2 - Brown
+var skin_color:int = 0
+var head_id:int = 0
+var clothes_id:int = 0
+var has_aapron:int = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	clear_character()
+	if character != 0:
+		generate_character()
+
+# Draws character from collection of assets
+func generate_character():
+	all_hands.visible = true
+	skin_color = randi() % 3
+	all_hands.frame = skin_color
+	if character == 1:
+		draw_cultist()
+	elif character > 1 and character < 5:
+		draw_hero()
+	elif character == 5:
+		draw_peon()
+
+func draw_cultist():
+	cult_heads.visible = true
+	head_id = randi() % 4
+	cult_heads.frame = ((head_id * 3) + skin_color)
+	all_body.frame = 1
+
+func draw_hero():
+	hero_heads.visible = true
+	if character == 2:
+		draw_hero_guard()
+	elif character == 3:
+		draw_hero_archer()
+	elif character == 4:
+		draw_hero_wizard()
+
+func draw_hero_guard():
+	hero_heads.visible = true
+	head_id = randi() % 4
+	print("skin: ", skin_color)
+	print("head: ", head_id)
+	hero_heads.frame = skin_color
+	clothes_id = randi() % 2
+	all_body.frame = 2 + clothes_id
+
+func draw_hero_archer():
+	hero_heads.visible = true
+	head_id = randi() % 4
+	print("skin: ", skin_color)
+	print("head: ", head_id)
+	hero_heads.frame = ((head_id * 3) + skin_color)
+	clothes_id = randi() % 2
+	all_body.frame = 4 + clothes_id
+
+func draw_hero_wizard():
+	hero_heads.visible = true
+	head_id = 0
+	hero_heads.frame = skin_color
+	clothes_id = randi() % 2
+	all_body.frame = 6 + clothes_id
+
+func draw_peon():
+	peon_heads.visible = true
+	head_id = randi() % 4
+	peon_heads.frame = ((head_id * 3) + skin_color)
+	clothes_id = randi() % 6
+	all_body.frame = 8 + clothes_id
+	has_aapron = randi() % 2
+	peon_aapron.visible = has_aapron
+
+func clear_character():
+	all_body.visible = true
+	all_hands.visible = false
+	cult_heads.visible = false
+	hero_heads.visible = false
+	peon_heads.visible = false
+	peon_aapron.visible = false
+	all_hands.frame = 0
+	cult_heads.frame = 0
+	hero_heads.frame = 0
+	peon_heads.frame = 0
+	
