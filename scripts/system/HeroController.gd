5c038b94cbf7c98eb1e87e1cad0ec66cbe38d2b9
unknown <michaelgardone@gmail.com>
2024-04-20 15:10:06 -0600

    fixed all heroes flashing bug

--- scripts/system/HeroController.gd
+++ scripts/system/HeroController.gd
@@ -11,11 +11,11 @@ const FX_EXPLODE_BLOOD = preload("res://prefabs/particles/fx_explode_blood.tscn"
 func spawn_hero(start_position, is_melee:bool):
 	var loaded:Adventurer
 	if is_melee:
-		loaded = MeleeHeroPrefab.duplicate().instantiate()
+		loaded = MeleeHeroPrefab.duplicate(true).instantiate()
 		add_child(loaded)
 		loaded.target = Player
 	else:
-		loaded = RangedHeroPrefab.duplicate().instantiate()
+		loaded = RangedHeroPrefab.duplicate(true).instantiate()
 		add_child(loaded)
 		loaded.target = Fleshball if Fleshball != null else Player
 		loaded.ball = Fleshball
