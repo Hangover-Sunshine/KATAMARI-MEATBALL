d58d8f62f778f2a6d98efdf220d680e051ef1f54
SBrianZ <brian@brianzavala.com>
2024-04-21 02:12:31 -0700

    splash fix?

--- scripts/menus/Splash_Menu.gd
+++ scripts/menus/Splash_Menu.gd
@@ -2,4 +2,5 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$TextFlash_Player.play("Button_Flash")
+	#$TextFlash_Player.play("Button_Flash")
+	pass
