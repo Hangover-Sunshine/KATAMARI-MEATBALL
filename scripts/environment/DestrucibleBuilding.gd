c823c09490c0f5a4ecddbe9750f77a7a0e4550e3
unknown <michaelgardone@gmail.com>
2024-04-23 01:14:58 -0600

    added breaking wall sfx, upgraded all versions to 1.1

--- scripts/environment/DestrucibleBuilding.gd
+++ scripts/environment/DestrucibleBuilding.gd
@@ -5,14 +5,23 @@ class_name DestructibleBuilding
 @export var MinVelocityThreshold:float = 3500
 @export_range(0.01, 1) var VelocityReduction:float = 0.6
 
+@onready var audio_stream_player = $AudioStreamPlayer
+
+func _input(event):
+	if event.is_action_pressed("Pause"):
+		audio_stream_player.play()
+	##
+##
+
 func destroy():
+	audio_stream_player.play()
 	# can't interact with the world
 	collision_layer = 0
 	collision_mask = 0
 	# play the destruction particle
 	# hide the actual sprite
 	$Wall.visible = false
-	process_mode = Node.PROCESS_MODE_DISABLED
+	#process_mode = Node.PROCESS_MODE_DISABLED
 	# show the destroyed ruins
 ##
 
